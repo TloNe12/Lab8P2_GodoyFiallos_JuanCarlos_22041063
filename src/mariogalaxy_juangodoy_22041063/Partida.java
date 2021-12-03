c072a13ea67752acc3dfd5d4c061a3d2c45fc275
TloNe12 <j.godoyagapehonduras@gmail.com>
2021-12-03 14:26:19 -0600

    Ya las clases creadas

---
+++ src/mariogalaxy_juangodoy_22041063/Partida.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mariogalaxy_juangodoy_22041063;
+
+/**
+ *
+ * @author jcgol
+ */
+public class Partida {
+    private String nombre;
+
+    public Partida(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
