d817ff2a2cea38fb413e47ff6c3d99cca5f6d01a
TloNe12 <j.godoyagapehonduras@gmail.com>
2021-12-03 15:14:32 -0600

    Clases finalizadas y Base de datos creada

--- src/mariogalaxy_juangodoy_22041063/Partida.java
+++ src/mariogalaxy_juangodoy_22041063/Partida.java
@@ -15,6 +15,19 @@ public class Partida {
     public Partida(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Partida{" + "nombre=" + nombre + '}';
+    }
     
     
     
