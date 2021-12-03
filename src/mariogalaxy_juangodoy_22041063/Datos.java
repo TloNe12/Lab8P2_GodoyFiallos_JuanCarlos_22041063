235ae2491c8cadd7c5afb2c6c942e5931d303d2c
TloNe12 <j.godoyagapehonduras@gmail.com>
2021-12-03 15:33:40 -0600

    Clases y Base de Datos finalizada

--- src/mariogalaxy_juangodoy_22041063/Datos.java
+++ src/mariogalaxy_juangodoy_22041063/Datos.java
@@ -5,10 +5,75 @@
  */
 package mariogalaxy_juangodoy_22041063;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jcgol
  */
 public class Datos {
-    private ArrayList<
+
+    private ArrayList<Partida> partida = new ArrayList();
+    private ArrayList<Estrella> estrella = new ArrayList();
+    private ArrayList<Jugador> jugador = new ArrayList();
+
+    //Crear Partidas 
+    public void crearPartida(String nombre) {
+        for (int i = 0; partida.size() > i; i++) {
+
+            if (partida.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                JOptionPane.showMessageDialog(null, "No se puede crear, ya existe");
+                return;
+            }
+            partida.add(new Partida(nombre));
+        }
+
+    }
+
+    public void editarPartida(String nombre) {
+        for (int i = 0; i < partida.size(); i++) {
+            if (partida.get(i).getNombre().equals(nombre)) {
+                partida.get(i).setNombre(nombre);
+                return;
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(null, "No se puedo modificar");
+
+    }
+
+    public void eliminarPartida(String nombre) {
+        for (int i = 0; i < partida.size(); i++) {
+            if (partida.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                partida.remove(i);
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "No se pudo eliminar");
+    }
+
+    public void agregarJugador(double velocidad, String nombre) {
+         for (int i = 0; jugador.size() > i; i++) {
+
+            if (jugador.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                JOptionPane.showMessageDialog(null, "No se puede crear, ya existe");
+                return;
+            }
+            jugador.add(new Jugador(velocidad,nombre));
+        }
+    }
+
+    public void agregarEstrella(double distancia, String descripcion, String nombre) {
+         for (int i = 0; estrella.size() > i; i++) {
+
+            if (estrella.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                JOptionPane.showMessageDialog(null, "No se puede crear, ya existe");
+                return;
+            }
+            estrella.add(new Estrella(distancia,descripcion,nombre));
+        }
+    }
+    
 }
--- src/mariogalaxy_juangodoy_22041063/Estrella.java
+++ src/mariogalaxy_juangodoy_22041063/Estrella.java
@@ -10,5 +10,46 @@ package mariogalaxy_juangodoy_22041063;
  * @author jcgol
  */
 public class Estrella {
+    private double distancia;
+    private String descripcion;
+    private String nombre;
+
+    public Estrella(double distancia, String descripcion, String nombre) {
+        this.distancia = distancia;
+        this.descripcion = descripcion;
+        this.nombre = nombre;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Estrella{" + "distancia=" + distancia + ", descripcion=" + descripcion + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
     
 }
